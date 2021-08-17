736659eb6b218ce1d3edfa66bed8be44e186e083
MaxwellYu <yuichiuyu1915@gmail.com>
2021-08-17 21:50:13 +0800

    deploy testing

--- src/components/Meals/MealItems/MealItem.js
+++ src/components/Meals/MealItems/MealItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 
 import styles from './MealItem.module.css';
 import PriceButton from './PriceButton';
--- src/components/UI/Image.js
+++
@@ -1,8 +0,0 @@
-import placeholder from '../../assets/pizza.jpg'
-
-export default function Image (props) {
-  // let icon = props.image ? `../../assets/${props.image}` : '../../assets/pizza.jpg';
-
-  return <img src={ require("" + props.image).default } alt={props.alt}>
-  </img>
-}
